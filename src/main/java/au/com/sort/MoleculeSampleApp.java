3539c591f1f2cc8dd339e91f17ac256709460a20
Robert Sutton <rlsutton1@optusnet.com.au>
2015-09-30 13:26:14 +1000

    improved atom selection by mouse click

--- src/main/java/au/com/sort/MoleculeSampleApp.java
+++ src/main/java/au/com/sort/MoleculeSampleApp.java
@@ -6,36 +6,32 @@ import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import javafx.scene.Camera;
+
 import javafx.application.Application;
 import javafx.event.EventHandler;
+import javafx.scene.Camera;
 import javafx.scene.Group;
-import javafx.scene.Node;
 import javafx.scene.PerspectiveCamera;
 import javafx.scene.Scene;
-import javafx.scene.input.MouseButton;
+import javafx.scene.SubScene;
+import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.PhongMaterial;
-import javafx.scene.shape.Box;
 import javafx.scene.shape.Sphere;
-import javafx.stage.Stage;
-import javafx.scene.control.*;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.SubScene;
-import javafx.scene.transform.Rotate;
 import javafx.scene.transform.Translate;
-import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
-
-public class MoleculeSampleApp extends Application {
+public class MoleculeSampleApp extends Application
+{
 
 	private static final int SEPARATION = 18;
 	private static List<Atom> primary;
 	private static List<Atom> secondary;
-	
+
 	final XForm world = new XForm();
 
 	final PerspectiveCamera camera = new PerspectiveCamera(true);
@@ -55,24 +51,25 @@ public class MoleculeSampleApp extends Application {
 	private PhongMaterial redMaterial;
 	private PhongMaterial greyMaterial;
 
-	private void buildScene(Group root) {
+	private void buildScene(Group root)
+	{
 		System.out.println("buildScene");
 		root.getChildren().add(world);
 	}
 
 	@Override
-	public void start(Stage primaryStage) {
+	public void start(Stage primaryStage)
+	{
 		System.out.println("start");
 		final Group root = new Group();
-				buildScene( root);
+		buildScene(root);
 
-		buildCamera( root);
+		buildCamera(root);
 		// buildAxes();
 		buildMolecule();
 
-	
-	//	SubScene subScene = new SubScene(subroot, 100, 200);
-		
+		// SubScene subScene = new SubScene(subroot, 100, 200);
+
 		Scene scene = new Scene(root, 1024, 768, true);
 		scene.setFill(Color.GREY);
 
@@ -80,42 +77,42 @@ public class MoleculeSampleApp extends Application {
 		primaryStage.setScene(scene);
 		primaryStage.show();
 
-
 		scene.setCamera(camera);
-		
-		//root.getChildren().add(buildSubScene());
-		//root.getChildren().add(test());
 
-		handleMouse(scene, root);
+		// root.getChildren().add(buildSubScene());
+		// root.getChildren().add(test());
+
+		handleMouse(scene);
 
 	}
-	
+
 	BorderPane test()
 	{
 		Button button = new Button("Brute");
 		BorderPane borderPane = new BorderPane();
-        borderPane.setStyle("-fx-border-color: black;-fx-background-color: #66CCFF;");
-        borderPane.setTop(button);
-        
-        borderPane.setMaxSize(2, 2);
-        
+		borderPane
+				.setStyle("-fx-border-color: black;-fx-background-color: #66CCFF;");
+		borderPane.setTop(button);
 
-        return borderPane;
+		borderPane.setMaxSize(2, 2);
+
+		return borderPane;
 	}
 
-	SubScene buildSubScene() {
+	SubScene buildSubScene()
+	{
 
 		Camera camera = new PerspectiveCamera();
 
-	     //-------------------
-//	    cameraXRotate = new Rotate(-5,Rotate.X_AXIS);
-//	    cameraYRotate = new Rotate(-50,Rotate.Y_AXIS);
-	    Translate cameraPosition2 = new Translate(0,10,10);    
-//
-//	    camera.getTransforms().add(cameraXRotate);
-//	    camera.getTransforms().add(cameraYRotate);
-//	    camera.getTransforms().add(cameraPosition2);
-		
+		// -------------------
+		// cameraXRotate = new Rotate(-5,Rotate.X_AXIS);
+		// cameraYRotate = new Rotate(-50,Rotate.Y_AXIS);
+		Translate cameraPosition2 = new Translate(0, 10, 10);
+		//
+		// camera.getTransforms().add(cameraXRotate);
+		// camera.getTransforms().add(cameraYRotate);
+		// camera.getTransforms().add(cameraPosition2);
+
 		HBox hbox1 = new HBox();
 		Button button = new Button("Brute");
 		hbox1.getChildren().add(button);
@@ -123,16 +120,16 @@ public class MoleculeSampleApp extends Application {
 		AnchorPane subroot = new AnchorPane();
 
 		AnchorPane.setRightAnchor(hbox1, 0.0);
-		AnchorPane.setBottomAnchor(hbox1,0.0);
+		AnchorPane.setBottomAnchor(hbox1, 0.0);
 
 		subroot.getChildren().add(hbox1);
 
 		SubScene subScene = new SubScene(subroot, 100, 200);
 		subScene.setCamera(camera);
-		
-//		subScene.setScaleX(0.2);
-//		subScene.setScaleY(0.2);
-//		subScene.setScaleZ(0.2);
+
+		// subScene.setScaleX(0.2);
+		// subScene.setScaleY(0.2);
+		// subScene.setScaleZ(0.2);
 
 		return subScene;
 	}
@@ -150,10 +147,12 @@ public class MoleculeSampleApp extends Application {
 	 * @throws InterruptedException
 	 */
 	public static void main(String[] args) throws FileNotFoundException,
-			IOException, InterruptedException {
+			IOException, InterruptedException
+	{
 		System.setProperty("prism.dirtyopts", "false");
 		String stripList = null;
-		if (args.length > 4) {
+		if (args.length > 4)
+		{
 			stripList = args[3];
 		}
 		String atomFilter = null;
@@ -162,7 +161,8 @@ public class MoleculeSampleApp extends Application {
 			atomFilter = args[2];
 		}
 
-		FileLoader loader = new FileLoader(args[0], args[1], atomFilter, stripList);
+		FileLoader loader = new FileLoader(args[0], args[1], atomFilter,
+				stripList);
 
 		primary = loader.getPrimary();
 		secondary = loader.getSecondary();
@@ -171,7 +171,8 @@ public class MoleculeSampleApp extends Application {
 		launch(args);
 	}
 
-	private void buildCamera(Group root) {
+	private void buildCamera(Group root)
+	{
 		root.getChildren().add(cameraXForm);
 		cameraXForm.getChildren().add(cameraXForm2);
 		cameraXForm2.getChildren().add(cameraXForm3);
@@ -213,7 +214,8 @@ public class MoleculeSampleApp extends Application {
 	// world.getChildren().addAll(axisGroup);
 	// }
 
-	private void buildMolecule() {
+	private void buildMolecule()
+	{
 
 		redMaterial = new PhongMaterial();
 		redMaterial.setDiffuseColor(Color.DARKRED);
@@ -231,7 +233,8 @@ public class MoleculeSampleApp extends Application {
 
 		XForm moleculeXForm2 = new XForm();
 
-		for (Atom m : primary) {
+		for (Atom m : primary)
+		{
 			XForm atomXForm = new XForm();
 
 			Sphere atom = new Sphere(1.0);
@@ -243,9 +246,11 @@ public class MoleculeSampleApp extends Application {
 
 			primaryAtoms.add(atom);
 			moleculeXForm1.getChildren().add(atomXForm);
+			addPrimaryMouseHandler(atom);
 		}
 
-		for (Atom m : secondary) {
+		for (Atom m : secondary)
+		{
 			XForm atomXForm = new XForm();
 
 			Sphere atom = new Sphere(1.0);
@@ -256,6 +261,7 @@ public class MoleculeSampleApp extends Application {
 			atomXForm.getChildren().add(atom);
 			moleculeXForm2.getChildren().add(atomXForm);
 			secondaryAtoms.add(atom);
+			addSecondaryMouseHandler(atom);
 		}
 
 		moleculeGroupPrimary.setTranslateX(-SEPARATION);
@@ -276,62 +282,66 @@ public class MoleculeSampleApp extends Application {
 
 	Map<Atom, Atom> selected = new LinkedHashMap<>();
 
-	private void handleMouse(Scene scene, final Node root) {
-
-		moleculeGroupSecondary
-				.setOnMousePressed(new EventHandler<MouseEvent>() {
-
-					@Override
-					public void handle(MouseEvent me) {
-						double x = me.getX();
-						double y = me.getY();
-						double z = me.getZ();
-
-						if (lastSelected != null && me.isSecondaryButtonDown()) {
-							// here we can do a relationship swap!!!
-
-							for (int i = 0; i < secondary.size(); i++) {
-								Atom m = secondary.get(i);
-								if (Math.abs((m.position.getX()) - x) < 1
-										&& Math.abs(m.position.getY() - y) < 1
-										&& Math.abs(m.position.getZ() - z) < 1) {
-
-									// clear the colors of the currently
-									// selected atoms
-									secondaryAtoms.get(lastSelected)
-											.setMaterial(redMaterial);
-									secondaryAtoms.get(i).setMaterial(
-											whiteMaterial);
-
-									// swap the secondary atoms around
-									Atom last = secondary.get(lastSelected);
-									Atom other = secondary.get(i);
-									secondary.add(lastSelected, other);
-									secondary.remove(lastSelected + 1);
-									secondary.add(i, last);
-									secondary.remove(i + 1);
-
-									Sphere lastAtom = secondaryAtoms
-											.get(lastSelected);
-									Sphere otherAtom = secondaryAtoms.get(i);
-
-									secondaryAtoms.add(lastSelected, otherAtom);
-									secondaryAtoms.remove(lastSelected + 1);
-									secondaryAtoms.add(i, lastAtom);
-									secondaryAtoms.remove(i + 1);
-
-									break;
-								}
-							}
-
-						}
-
-					}
-				});
-		moleculeGroupPrimary.setOnMousePressed(new EventHandler<MouseEvent>() {
+	private void addSecondaryMouseHandler(final Sphere node)
+	{
+		node.setOnMousePressed(new EventHandler<MouseEvent>()
+		{
 
 			@Override
-			public void handle(MouseEvent me) {
+			public void handle(MouseEvent me)
+			{
+
+				if (lastSelected != null && me.isSecondaryButtonDown())
+				{
+					// here we can do a relationship swap!!!
+
+					for (int i = 0; i < secondaryAtoms.size(); i++)
+					{
+						Sphere m = secondaryAtoms.get(i);
+
+						if (m == node)
+						{
+
+							// clear the colors of the currently
+							// selected atoms
+							secondaryAtoms.get(lastSelected).setMaterial(
+									redMaterial);
+							secondaryAtoms.get(i).setMaterial(whiteMaterial);
+
+							// swap the secondary atoms around
+							Atom last = secondary.get(lastSelected);
+							Atom other = secondary.get(i);
+							secondary.add(lastSelected, other);
+							secondary.remove(lastSelected + 1);
+							secondary.add(i, last);
+							secondary.remove(i + 1);
+
+							Sphere lastAtom = secondaryAtoms.get(lastSelected);
+							Sphere otherAtom = secondaryAtoms.get(i);
+
+							secondaryAtoms.add(lastSelected, otherAtom);
+							secondaryAtoms.remove(lastSelected + 1);
+							secondaryAtoms.add(i, lastAtom);
+							secondaryAtoms.remove(i + 1);
+
+							break;
+						}
+					}
+
+				}
+
+			}
+		});
+	}
+
+	void addPrimaryMouseHandler(final Sphere node)
+	{
+		node.setOnMousePressed(new EventHandler<MouseEvent>()
+		{
+
+			@Override
+			public void handle(MouseEvent me)
+			{
 				mousePosX = me.getSceneX();
 				mousePosY = me.getSceneY();
 				mouseOldX = me.getSceneX();
@@ -340,25 +350,24 @@ public class MoleculeSampleApp extends Application {
 				System.out.println(me.getX() + " " + me.getY() + "  "
 						+ me.getZ());
 
-				double x = me.getX();
-				double y = me.getY();
-				double z = me.getZ();
-
-				if (me.getClickCount() == 1) {
-					if (lastSelected != null) {
+				if (me.getClickCount() == 1)
+				{
+					if (lastSelected != null)
+					{
 						PhongMaterial material = greyMaterial;
-						if (selected.get(primary.get(lastSelected)) == null) {
+						if (selected.get(primary.get(lastSelected)) == null)
+						{
 							material = redMaterial;
 						}
 						primaryAtoms.get(lastSelected).setMaterial(material);
 						secondaryAtoms.get(lastSelected).setMaterial(material);
 					}
 
-					for (int i = 0; i < primary.size(); i++) {
-						Atom m = primary.get(i);
-						if (Math.abs((m.position.getX()) - x) < 1
-								&& Math.abs(m.position.getY() - y) < 1
-								&& Math.abs(m.position.getZ() - z) < 1) {
+					for (int i = 0; i < primary.size(); i++)
+					{
+						Sphere m = primaryAtoms.get(i);
+						if (m == node)
+						{
 							primaryAtoms.get(i).setMaterial(whiteMaterial);
 							secondaryAtoms.get(i).setMaterial(whiteMaterial);
 							System.out.println("Matched");
@@ -368,10 +377,13 @@ public class MoleculeSampleApp extends Application {
 					}
 				}
 
-				if (me.getClickCount() == 2) {
+				if (me.getClickCount() == 2)
+				{
 					if (lastSelected != null
-							&& primary.get(lastSelected) != null) {
-						if (selected.get(primary.get(lastSelected)) == null) {
+							&& primary.get(lastSelected) != null)
+					{
+						if (selected.get(primary.get(lastSelected)) == null)
+						{
 							selected.put(primary.get(lastSelected),
 									secondary.get(lastSelected));
 							System.out.println("double");
@@ -380,7 +392,8 @@ public class MoleculeSampleApp extends Application {
 							secondaryAtoms.get(lastSelected).setMaterial(
 									greyMaterial);
 
-						} else {
+						} else
+						{
 							{
 								selected.remove(primary.get(lastSelected));
 								System.out.println("double");
@@ -394,23 +407,31 @@ public class MoleculeSampleApp extends Application {
 					}
 				}
 
-				for (Atom t : selected.keySet()) {
+				for (Atom t : selected.keySet())
+				{
 					System.out.println(t + " " + selected.get(t));
 				}
 
 			}
 		});
-		scene.setOnMouseDragged(new EventHandler<MouseEvent>() {
+	}
+
+	private void handleMouse(Scene scene)
+	{
+
+		scene.setOnMouseDragged(new EventHandler<MouseEvent>()
+		{
 			private double mouseDeltaX;
 			private double mouseDeltaY;
 
 			@Override
-			public void handle(MouseEvent me) {
+			public void handle(MouseEvent me)
+			{
 				if (!me.isStillSincePress())
 				{
-				mouseOldX = mousePosX;
-				mouseOldY = mousePosY;
-				}else
+					mouseOldX = mousePosX;
+					mouseOldY = mousePosY;
+				} else
 				{
 					mouseOldX = me.getSceneX();
 					mouseOldY = me.getSceneY();
@@ -424,13 +445,16 @@ public class MoleculeSampleApp extends Application {
 				double modifier = 1.0;
 				double modifierFactor = 0.1;
 
-				if (me.isControlDown()) {
+				if (me.isControlDown())
+				{
 					modifier = 0.1;
 				}
-				if (me.isShiftDown()) {
+				if (me.isShiftDown())
+				{
 					modifier = 10.0;
 				}
-				if (me.isPrimaryButtonDown()) {
+				if (me.isPrimaryButtonDown())
+				{
 					moleculeGroupPrimary.ry.setAngle(moleculeGroupPrimary.ry
 							.getAngle()
 							- mouseDeltaX
@@ -451,11 +475,13 @@ public class MoleculeSampleApp extends Application {
 									+ mouseDeltaY * modifierFactor * modifier
 									* 2.0); // -
 
-				} else if (me.isSecondaryButtonDown()) {
+				} else if (me.isSecondaryButtonDown())
+				{
 					double z = camera.getTranslateZ();
 					double newZ = z + mouseDeltaX * modifierFactor * modifier;
 					camera.setTranslateZ(newZ);
-				} else if (me.isMiddleButtonDown()) {
+				} else if (me.isMiddleButtonDown())
+				{
 					cameraXForm2.t.setX(cameraXForm2.t.getX() + mouseDeltaX
 							* modifierFactor * modifier * 0.3); // -
 					cameraXForm2.t.setY(cameraXForm2.t.getY() + mouseDeltaY
